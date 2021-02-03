6570b5941b353c0174f66a59032aed4bb44c0db4
jordisan <jordisan@users.noreply.github.com>
2021-02-03 10:59:18 +0000

    deploy: 558e2ee3ce1df842a25aa13832bc3bf588926757

---
+++ _nuxt/827e3e1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{163:function(n,t,e){"use strict";var c=e(30),component=Object(c.a)({},(function(){var n=this.$createElement,t=this._self._c||n;return t("div",[t("Nuxt")],1)}),[],!1,null,null,null);t.a=component.exports},171:function(n,t,e){e(172),n.exports=e(173)}},[[171,6,3,7]]]);
